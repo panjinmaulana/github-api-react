d1028878dd3ce63f1fc688e3c4685e477a6cf21d
Panji Maulana <panjinmaulana@gmail.com>
2022-08-03 18:34:46 +0700

    add font poppins & square peg, fix card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 export default function Card({ user }) {
     return (
-        <div className="text-center py-5">
+        <div className="text-center pt-5">
             <h5>Profile</h5>
-            <div className="card mx-auto" style={{ width: "18rem", border: "none" }}>
+            <div className="card mx-auto" style={{ width: "20rem", border: "none" }}>
                 <img
                     src={user.avatar_url}
-                    className="card-img-top rounded-circle"
+                    className="card-img-top rounded-circle mx-auto"
                     alt={user.login}
+                    style={{width: "240px"}}
                 />
                 <div className="card-body">
                     <h5 className="card-title text-center">{user.name || user.login}</h5>
